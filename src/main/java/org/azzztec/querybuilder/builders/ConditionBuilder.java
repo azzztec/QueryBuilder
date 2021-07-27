aea8186cda08a3201cea2bf0d3bf5ad36bf88582
azzztec <33kaxa@gmail.com>
2021-07-27 21:34:03 +0300

    remove extra space after predicate

--- src/main/java/org/azzztec/querybuilder/builders/ConditionBuilder.java
+++ src/main/java/org/azzztec/querybuilder/builders/ConditionBuilder.java
@@ -12,7 +12,7 @@ import java.util.Queue;
 
 public class ConditionBuilder<T> {
     private List<Triplet<String, Object, Condition>> conditions = new ArrayList<>();
-    private Queue<LogicOperator> logicOperators = new LinkedList<>();
+    private Queue<LogicOperator> predicates = new LinkedList<>();
 
     public T where(String key, Object value, Condition condition) {
         conditions.add(new Triplet(key, value, condition));
@@ -20,12 +20,12 @@ public class ConditionBuilder<T> {
     }
 
     public T and() {
-        logicOperators.add(LogicOperator.AND);
+        predicates.add(LogicOperator.AND);
         return (T) this;
     }
 
     public T or() {
-        logicOperators.add(LogicOperator.OR);
+        predicates.add(LogicOperator.OR);
         return (T) this;
     }
 
@@ -34,13 +34,13 @@ public class ConditionBuilder<T> {
         StringBuilder res = new StringBuilder("");
         if(!conditions.isEmpty()) {
             try {
-                if(logicOperators.size() == conditions.size() - 1 || logicOperators.isEmpty()) {
+                if(predicates.size() == conditions.size() - 1 || predicates.isEmpty()) {
                     res.append(" WHERE");
                     for (int i = 0; i < conditions.size(); i++) {
                         tripl = conditions.get(i);
                         res.append(" " + tripl.getValue0() + tripl.getValue2().getCharacter() + "'" + tripl.getValue1() + "'");
-                        if (!logicOperators.isEmpty()) {
-                            res.append(" " + logicOperators.poll() + " ");
+                        if (!predicates.isEmpty()) {
+                            res.append(" " + predicates.poll());
                         }
                     }
                 } else {
